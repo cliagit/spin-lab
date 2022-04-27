55c8676d52b7addd458093e17a7c5ee9d8eb88a8
carmine <lcarmi@libero.it>
2022-04-27 15:48:26 +0200

    Spostamento degli script dalla cartella degli Esperimenti in quella base

--- exp_ec_v_source.py
+++ exp_ec_v_source.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
 '''
- Electrical characteristic experiment with current source.
- Keithely Source meter as fixed or variable current generator and
- Nano Voltmeter as voltage reader on channel 1
- Keithely multimeter 2000 or 2700 to measure temperature with silicon diode DT470
+ Electrical characteristic experiment with voltage source
+ Keithely Source 2400 meter as fixed or variable voltage generator and
+ current meter; Keithely multimeter 2000 or 2700 to measure temperature with silicon diode DT470
 '''
 from datetime import datetime
 from time import sleep
-from scipy import signal
 import threading
 import configparser
 import sys
 import os
 import shutil
 import logging
+from scipy import signal
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import easygui as eg
@@ -45,7 +44,7 @@ AREA = conf.getfloat('AREA')
 LENGTH = conf.getfloat('LENGTH')
 # Numero di valori sui quali fare la media
 AVG_MEASURE = conf.getint('AVG_MEASURE')
-# Current number of samples
+# Source number of samples
 SOURCE_SAMPLES = conf.getint('SOURCE_SAMPLES')
 SOURCE_FLIPPED = conf.getboolean('SOURCE_FLIPPED')
 
@@ -57,29 +56,30 @@ DISPLAY_SAMPLES = int(SOURCE_SAMPLES * 1.15)
 # Select fixed or variable source
 if conf.getboolean('SOURCE_FIXED'):
     SOURCE_FLIPPED = False
-    # Current fixed source
-    SOURCE_I =  np.ones(SOURCE_SAMPLES) * conf.getfloat('SOURCE_FIXED_VALUE')
-    title = f"{SAMPLE_NAME} at fixed current {conf['SOURCE_FIXED_VALUE']}A"
+    # Fixed source
+    SOURCE =  np.ones(SOURCE_SAMPLES) * conf.getfloat('SOURCE_FIXED_VALUE')
+    title = f"{SAMPLE_NAME} at fixed voltage {conf['SOURCE_FIXED_VALUE']}V"
 else:
     if conf.getboolean('SOURCE_SQUARE_WAVE'):
         SOURCE_FLIPPED = False
         SOURCE_SQUARE_VALUE = conf.getfloat('SOURCE_SQUARE_VALUE')
         SOURCE_SQUARE_PERIOD = conf.getint('SOURCE_SQUARE_PERIOD')
         t = np.linspace(0, 1, SOURCE_SAMPLES)
-        SOURCE_I = signal.square(( 2 * np.pi * SOURCE_SQUARE_PERIOD *t )) * SOURCE_SQUARE_VALUE
-        title = f"{SAMPLE_NAME} current square waveform value {SOURCE_SQUARE_VALUE}A"
+        SOURCE = signal.square(( 2 * np.pi * SOURCE_SQUARE_PERIOD *t )) * SOURCE_SQUARE_VALUE
+        title = f"{SAMPLE_NAME} voltage square waveform value {SOURCE_SQUARE_VALUE}V"
     else:
-        # Current min source
-        SOURCE_MIN_VALUE = conf.getfloat('SOURCE_MIN_VALUE')
-        # Current max source
-        SOURCE_MAX_VALUE = conf.getfloat('SOURCE_MAX_VALUE')
-        # Current array of num sample from start to end equally spaced
-        SOURCE_I = np.linspace(SOURCE_MIN_VALUE, SOURCE_MAX_VALUE, SOURCE_SAMPLES)
-        title = f"{SAMPLE_NAME} current from {conf['SOURCE_MIN_VALUE']} to {conf['SOURCE_MAX_VALUE']}A"
+        # Variable min source
+        SOURCE_VAR_MIN_VALUE = conf.getfloat('SOURCE_VAR_MIN_VALUE')
+        # Variable max source
+        SOURCE_VAR_MAX_VALUE = conf.getfloat('SOURCE_VAR_MAX_VALUE')
+        # Variable array of num sample from start to end equally spaced
+        SOURCE = np.linspace(SOURCE_VAR_MIN_VALUE, SOURCE_VAR_MAX_VALUE, SOURCE_SAMPLES)
+        title = f"{SAMPLE_NAME} voltage from {conf['SOURCE_VAR_MIN_VALUE']} \
+to {conf['SOURCE_VAR_MAX_VALUE']}V"
 
 # Append flipped current array of num sample from end to start
 if SOURCE_FLIPPED:
-    SOURCE_I = np.concatenate((SOURCE_I, np.flip(SOURCE_I)))
+    SOURCE = np.concatenate((SOURCE, np.flip(SOURCE)))
     DISPLAY_SAMPLES *= 2
     title += ' flipped'
     logging.info('Source is flipped')
@@ -110,80 +110,33 @@ except gpib.GpibError as e:
     print("Multimeter doesn't respond, check it out!", e)
     sys.exit(-1)
 
-
-### Configurazione del nano voltmeter Keithley 2182A
-# Port GPIB 0, GPIB Intrument address 7
-try:
-    nanovolt=Gpib.Gpib(0,7)
-    # Reset GPIB defaults
-    nanovolt.write("*RST")
-    # Identify request
-    nanovolt.write("*IDN?")
-    # Read answer
-    logging.info('Found Nanovolt Meter %s', nanovolt.read().decode("utf-8"))
-    # print(nanovolt.read().decode('utf-8'))
-    # Select source function, mode Voltage reading only.
-    nanovolt.write(":SENS:FUNC 'VOLT'")
-    # CHANNEL 1
-    nanovolt.write(":SENS:CHAN 1")
-except gpib.GpibError as e:
-    logging.fatal("Nanovolt meter doesn't respond: %s" , e)
-    print("Nanovolt meter doesn't respond, check it out!", e)
-    sys.exit(-1)
-
 ### Configurazione del SourceMeter Keithley 2400
 # Port GPIB 0, GPIB Intrument address 24
-#try:
-#    sm=Gpib.Gpib(0,24)
-#    # Reset GPIB defaults
-#    sm.write("*RST")
-#    # Identify request
-#    sm.write("*IDN?")
-#    # Read answer from device
-#    logging.info('Found Source Meter %s', sm.read().decode("utf-8"))
-#    # print(sm.read().decode('utf-8'))
-#### Select source function, mode '''
-#    #Select current source.
-#    sm.write(":SOUR:FUNC CURR")
-#    # Select source range.
-#    #sm.write(":SOUR:CURR:RANG 10E-3")
-#    # Source output.
-#    sm.write(f":SOUR:CURR:LEV {SOURCE_I[0]}")
-#    # Voltage compliance.
-#    sm.write(f':SENS:VOLT:PROT {conf["LIMIT"]}')
-#    # Voltage measure function.
-#    sm.write(":SENS:FUNC 'VOLT'")
-#    # Voltage reading only.
-#    sm.write(":FORM:ELEM VOLT")
-#    # Turn on source meter output
-#    sm.write(":OUTP ON")
-#except gpib.GpibError as e:
-#    logging.fatal("Source meter 2400 doesn't respond: %s", e)
-#    print("Source meter doesn't respond, check it out!", e)
-#    sys.exit(-1)
-
-### Configurazione del SourceMeter Keithley 6220
-# Port GPIB 0, GPIB Intrument address 12
 try:
-    sm=Gpib.Gpib(0,12)
+    sm=Gpib.Gpib(0,24)
     # Reset GPIB defaults
     sm.write("*RST")
     # Identify request
     sm.write("*IDN?")
-    # Read answer
+    # Read answer from device
     logging.info('Found Source Meter %s', sm.read().decode("utf-8"))
-    sm.write(":CLE")
-    # Select source range.
-    sm.write(":SOUR:CURR:RANG:AUTO ON")
+    # print(sm.read().decode('utf-8'))
+### Select source function, mode '''
+    #Select voltage source.
+    sm.write(":SOUR:FUNC VOLT")
     # Source output.
-    sm.write(f":SOUR:CURR {SOURCE_I[0]}")
-    # Compliance.voltage limit
-    sm.write(f':SOUR:CURR:COMP {conf["LIMIT"]}')
-    # Turn on output
+    sm.write(f":SOUR:VOLT:LEV {SOURCE[0]}")
+    # Current compliance.
+    sm.write(f':SENS:CURR:PROT {conf["LIMIT"]}')
+    # Current measure function.
+    sm.write(":SENS:FUNC 'CURR'")
+    # Current reading only.
+    sm.write(":FORM:ELEM CURR")
+    # Turn on source meter output
     sm.write(":OUTP ON")
 except gpib.GpibError as e:
-    logging.fatal("Source meter 6220 doesn't respond: %s", e)
-    print("Source meter 6220 doesn't respond, check it out!", e)
+    logging.fatal("Source meter 2400 doesn't respond: %s", e)
+    print("Source meter doesn't respond, check it out!", e)
     sys.exit(-1)
 
 # Instantiate threading event handler
@@ -220,16 +173,16 @@ def measure_thread_function():
     global J
     J = []
     if continuous_mode:
-        eg.msgbox('Start new measurement loop in continuous mode; to stop and save the measurements close the plot \
-window')
+        eg.msgbox('Start new measurement loop in continuous mode; \
+to stop and save the measurements close the plot window')
         logging.info("Start the measurement loop in continuous mode")
     else:
         logging.info("Start the measurement loop")
     # Measurement loop
     while True:
         try:
-            # Impostazione del valore iniziale della corrente
-            sm.write(f":SOUR:CURR {SOURCE_I[0]}")
+            # Impostazione del valore iniziale del voltaggio
+            sm.write(f":SOUR:VOLT {SOURCE[0]}")
         except gpib.GpibError as e:
             logging.warning("Writing gpib error, check the source meter: %s", e)
             # print(f"Writing gpib error: {e}")
@@ -245,6 +198,8 @@ window')
                     # Read temperature
                     multimeter.write(':READ?')
                     tmp= dt400.voltage_to_temp(float(multimeter.read()))
+                except ValueError:
+                    logging.warning('Temperature out of range!')
                 except gpib.GpibError as e:
                     logging.warning("Reading gpib error, check the multimeter: %s", e)
                     # print(f"Reading error, check the instruments: {e}")
@@ -256,21 +211,19 @@ window')
             if not answer == 'Yes, go on':
                 break
         start_measurements = True
-        # nvolt_measure_prev = -1000.0
-        # Ciclo della corrente
-        for i in SOURCE_I:
+        # Ciclo della voltaggio
+        for volt in SOURCE:
             try:
-                # Thread exits, interruzione del ciclo della corrente
+                # Thread exits, interruzione del ciclo di misura
                 if exit_event.is_set():
-                    logging.info("Leaving the current loop")
-                    # print("Uscita ciclo di corrente")
+                    logging.info("Leaving the measurement loop")
                     break
 
-                # Impostazione della corrente.
-                sm.write(f":SOUR:CURR {i}")
-                logging.info("Measurement at current %s", i)
+                # Impostazione del voltaggio
+                sm.write(f":SOUR:VOLT {volt}")
+                logging.info("Measurement at voltage %s", volt)
                 error = False
-                volt_sum = 0.0
+                curr_sum = 0.0
                 temp_sum = 0.0
             except gpib.GpibError as e:
                 logging.warning("Writing gpib error, check the source meter: %s", e)
@@ -280,22 +233,15 @@ window')
             for _j in range(AVG_MEASURE):
                 # print(f"\nMisura :{_j}\n")
                 try:
-                    # Read Voltage with NanoVolt
-                    nanovolt.write(':READ?')
-                    nvolt_measure = float(nanovolt.read())
-                    volt_sum += nvolt_measure
-                    # sleep(DELAY)
+                    # Read current
+                    sm.write(':READ?')
+                    curr_measure = float(sm.read())
+                    curr_sum += curr_measure
+                    sleep(DELAY)
                     # Read temperature
                     multimeter.write(':READ?')
                     temp_measure = dt400.voltage_to_temp(float(multimeter.read()))
                     temp_sum += temp_measure
-#                   if abs(nvolt_measure - nvolt_measure_prev) > 1.0 and not nvolt_measure_prev < 0:
-#                        logging.warning("Voltage reading differ > 1V: current value is %sV \
-#previous %sV temperature %s°K current %sA", nvolt_measure, nvolt_measure_prev, temp_measure, i)
-                        # print(f"\nVoltage reading differ > 1V, current value is \
-#{nvolt_measure:.4f}V previous {nvolt_measure_prev:.4f}V temperature \
-#{temp_measure:.2f}°K current {i:.4e}A\n")
-                    #nvolt_measure_prev = nvolt_measure
                     sleep(DELAY)
                     error = False
                 except ValueError:
@@ -310,33 +256,22 @@ window')
                     break
             if not error:
                 temp = temp_sum/AVG_MEASURE
-                volt = volt_sum/AVG_MEASURE
-                res = volt/i
+                curr = curr_sum/AVG_MEASURE
+                res = volt/curr
                 e_field = volt/LENGTH
-                c_density = i/AREA
+                c_density = curr/AREA
                 rho = e_field/c_density
-            #            try:
-            #                # Lettura del voltaggio misurato al Source Meter 2400
-            #                sm.write(':READ?')
-            #                voltSm = float(sm.read())
-            #                lim = float(conf["LIMIT"])
-            #                # print(f'T:{temp:.2f}°K V:{volt:.3f} V R:{res:.3f} 𝛀
-            #                        Voltage limit: {(voltSm*100)/lim:.2f}%', end="\r")
-            #            except:
-            #                # print(f'T:{temp:.2f}°K V:{volt:.3f} V R:{res:.3f} 𝛀                ',
-            #                         end="\r")
-            #                # print("\nSource meter reading error!\n")
-                log_measure = f'T:{temp:.2f}°K V:{volt:.4e}V I:{i:.4e}A R:{res:.4e}𝛀 \
+                log_measure = f'T:{temp:.2f}°K V:{volt:.4e}V I:{curr:.4e}A R:{res:.4e}𝛀 \
 E:{e_field:.4e}V/cm J:{c_density:.4e}A/cm2 𝛒:{rho:.4e}𝛀 cm'
                 # print(f'T:{temp:.2f}°K V:{volt:.4e}V I:{i:.4e}A R:{res:.4e}𝛀 \
 #E:{e_field:.4e}V/cm J:{c_density:.4e}A/cm2 𝛒:{rho:.4e}𝛀 cm',
 #                end="\r")
                 logging.info('%s', log_measure)
-                if volt >= float(conf["LIMIT"]) - 0.1:
-                    logging.warning("Voltage compliance")
+                if curr >= float(conf["LIMIT"]) - 0.1:
+                    logging.warning("Current compliance")
                 else:
                     # Update current array
-                    I.append(i)
+                    I.append(curr)
                     # Update voltage array
                     V.append(volt)
                     # Update resistance array
@@ -360,7 +295,7 @@ thr_measure.start()
 ### Plotting ###
 fig, [ax0, ax1, ax2] = plt.subplots(3,1, figsize=(16, 12))
 ax0.set(ylabel='Resistance [Ohm]', xlabel='Time', title=title)
-ax1.set(ylabel='Voltage [V]', xlabel='Time')# , yscale='log', xscale='log')
+ax1.set(ylabel='Current [A]', xlabel='Time')# , yscale='log', xscale='log')
 ax2.set(ylabel='Temperature [°K]', xlabel='Time') # , yscale='log', xscale='log')
 
 ax0.grid()
@@ -394,17 +329,17 @@ def update_plot(i):
             ax2.cla()
             ax2.grid()
             ax0.set(ylabel='Resistance [Ohm]', xlabel='Time', title=title)
-            ax1.set(ylabel='Voltage [V]', xlabel='Time')# , yscale='log', xscale='log')
+            ax1.set(ylabel='Current [A]', xlabel='Time')# , yscale='log', xscale='log')
             ax2.set(ylabel='Temperature [°K]', xlabel='Time') # , yscale='log', xscale='log')
             #ax0.set_xlim([DT[N - DISPLAY_SAMPLES], DT[N-1]])
             #ax1.set_xlim([DT[N - DISPLAY_SAMPLES], DT[N-1]])
             #ax2.set_xlim([DT[N - DISPLAY_SAMPLES], DT[N-1]])
             ax0.plot(DT[N - DISPLAY_SAMPLES:-1], R[N - DISPLAY_SAMPLES:-1], '.-', color='orange')
-            ax1.plot(DT[N - DISPLAY_SAMPLES:-1], V[N - DISPLAY_SAMPLES:-1], '.-', color='red')
+            ax1.plot(DT[N - DISPLAY_SAMPLES:-1], I[N - DISPLAY_SAMPLES:-1], '.-', color='red')
             ax2.plot(DT[N - DISPLAY_SAMPLES:-1], T[N - DISPLAY_SAMPLES:-1], '.-', color='yellow')
         else:
             ax0.plot(DT[:i], R[:i], '.-', color='orange')
-            ax1.plot(DT[:i], V[:i], '.-', color='red')
+            ax1.plot(DT[:i], I[:i], '.-', color='red')
             ax2.plot(DT[:i], T[:i], '.-', color='yellow')
 
         if N > 0:
@@ -416,7 +351,7 @@ def update_plot(i):
             # Resistenza
             an0 = ax0.annotate(f'{R[-1]:.3e}', xy=(1.01, 0.9),  xycoords='axes fraction', color="w")
             # Voltaggio
-            an1 = ax1.annotate(f'{V[-1]:.3e}', xy=(1.01, 0.9),  xycoords='axes fraction', color="w")
+            an1 = ax1.annotate(f'{I[-1]:.3e}', xy=(1.01, 0.9),  xycoords='axes fraction', color="w")
             # Temperatura
             an2 = ax2.annotate(f'{T[-1]:.2e}', xy=(1.01, 0.9),  xycoords='axes fraction', color="w")
             ann_list.append(an0)
@@ -431,7 +366,7 @@ def on_close(event):
     date_time = datetime.now().strftime("%Y%m%d%H%M%S")
     answer = eg.ynbox('Save data?', 'Closing the experiment', ('Yes', 'No'))
     if answer and len(T) > 0:
-        path = SAMPLE_NAME + "/" + title.replace(" ", "_")
+        path = "Esperimenti/" + SAMPLE_NAME + "/" + title.replace(" ", "_")
         path_file = path + "/" + title.replace(" ", "_") + "-" + date_time
         try:
             # Creazione, se non esistente, della cartella base col nome del campione
@@ -448,15 +383,16 @@ def on_close(event):
                 file.write(f"\nArea: {conf['AREA']}cm2")
                 file.write(f"\nLength: {conf['LENGTH']}cm")
                 if conf.getboolean('SOURCE_FIXED'):
-                    file.write(f"\nCurrent source fixed at {conf['SOURCE_FIXED_VALUE']}A")
+                    file.write(f"\nVoltage source fixed at {conf['SOURCE_FIXED_VALUE']}V")
                 elif conf.getboolean('SOURCE_SQUARE_WAVE'):
-                   file.write(f"\nCurrent square waveform source, value {conf['SOURCE_SQUARE_VALUE']}A")
+                    file.write(f"\nVoltage square waveform source, value \
+{conf['SOURCE_SQUARE_VALUE']}V")
                 elif SOURCE_FLIPPED:
-                    file.write(f"\nCurrent source starts and ends at {conf['SOURCE_MIN_VALUE']}A \
-through {conf['SOURCE_MAX_VALUE']}A")
+                    file.write(f"\nVoltage source starts and ends at \
+{conf['SOURCE_VAR_MIN_VALUE']}V through {conf['SOURCE_VAR_MAX_VALUE']}V")
                 else:
-                    file.write(f"\nCurrent source from {conf['SOURCE_MIN_VALUE']}A to \
-{conf['SOURCE_MAX_VALUE']}A")
+                    file.write(f"\nVoltage source from {conf['SOURCE_VAR_MIN_VALUE']}V to \
+{conf['SOURCE_VAR_MAX_VALUE']}V")
                 file.write(f'\n\n### Experiment {date_time} ###')
                 file.write(f'\nDate {DT[0].strftime("%Y-%m-%d")} start at \
 {DT[0].strftime("%H:%M:%S")} end at {DT[-1].strftime("%H:%M:%S")} \
@@ -466,10 +402,10 @@ duration {str(DT[-1].replace(microsecond=0)-DT[0].replace(microsecond=0))}')
                 file.write(f'\n\t average {np.average(RHO):.4e}𝛀 cm')
                 file.write(f'\n\t minimum {np.min(RHO):.4e}𝛀 cm at {T[np.argmin(R)]:.2f}°K')
                 file.write(f'\n\t maximum {np.max(RHO):.4e}𝛀 cm at {T[np.argmax(R)]:.2f}°K')
-                file.write('\nVoltage:')
-                file.write(f'\n\t average {np.average(V):.4e}V')
-                file.write(f'\n\t minimum {np.min(V):.4e}V at {T[np.argmin(V)]:.2f}°K')
-                file.write(f'\n\t maximum {np.max(V):.4e}V at {T[np.argmax(V)]:.2f}°K')
+                file.write('\nCurrent:')
+                file.write(f'\n\t average {np.average(I):.4e}A')
+                file.write(f'\n\t minimum {np.min(I):.4e}A at {T[np.argmin(I)]:.2f}°K')
+                file.write(f'\n\t maximum {np.max(I):.4e}A at {T[np.argmax(I)]:.2f}°K')
         except OSError as error:
             logging.error("Error handling description file: %s", error)
             # print(error)
@@ -485,7 +421,7 @@ duration {str(DT[-1].replace(microsecond=0)-DT[0].replace(microsecond=0))}')
         csv_path = path_file + ".csv"
         logging.info("Save data in CSV format %s", csv_path)
         data = pd.DataFrame(np.stack((T, V, R, I, E, RHO, J), axis=-1),
-               columns=['Temperature [K]', 'Voltage [V]', 'Resistance [𝛀]', 'Current Source [A]', \
+               columns=['Temperature [K]', 'Voltage source [V]', 'Resistance [𝛀]', 'Current [A]', \
 'Electric Field [V/cm]', 'Restivity [𝛀 cm]', 'Current Density [A/cm2]'])
         data.to_csv(csv_path, index=False)
 
